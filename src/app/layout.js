8798788479a28e98f76c784ceec103e7b7d06fd8
ujjwal patel <newujjwalpatel@gmail.com>
2025-08-03 15:48:50 +0530

    adsense verify partc

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
-import Script from "next/script";
 import "./globals.css";
-
+import Script from "next/script";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -15,21 +14,17 @@ const geistMono = Geist_Mono({
 export const metadata = {
   title: "NapStopper",
   description: "NapStopper pings your backend every 10 minutes to keep it awake and responsive. Ideal for free-tier platforms like Render, Vercel, and Railway. Simple, reliable, and free to get started.",
-  other: {
-    "google-adsense-account": "ca-pub-1028044699975607"
-  }
 };
+
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <Script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1028044699975607"
-          crossOrigin="anonymous"
-          strategy="beforeInteractive"
-        />
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+      <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1028044699975607"
+     crossOrigin="anonymous"></Script>
         {children}
       </body>
     </html>
