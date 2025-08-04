701e490e561a7b8ca9e822dc6801fc2d5d30be6b
ujjwal patel <newujjwalpatel@gmail.com>
2025-08-04 10:57:30 +0530

    updated adsense parth

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Head from "next/head";
+import Script from "next/script";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -20,9 +20,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-         <Head>
-        <meta name="google-adsense-account" content="ca-pub-1028044699975607" />
-      </Head>
+    <head>
+    <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4150861973710697"
+     crossOrigin="anonymous"></Script>
+    </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
